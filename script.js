b9530ff5974d8a7f9c0adada8e9098054cc67479
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2022-05-28 18:45:26 +0100

    Added the Cart Modal

--- script.js
+++ script.js
@@ -73,3 +73,21 @@ function increment() {
     itemNumber.innerText = count;
 }
 
+
+var modal = document.getElementById("modal");
+var modalBtn = document.getElementById("modalBtn");
+var span = document.getElementsByClassName("close")[0];
+
+modalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
