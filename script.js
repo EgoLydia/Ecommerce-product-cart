ff427852e014ae9eecaaf7e2786a238f867a5eac
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2022-05-29 18:47:57 +0100

    .

--- script.js
+++ script.js
@@ -159,13 +159,14 @@ function addEvent() {
     });
   }
 
-  const close = document.querySelectorAll('.close')
-    for (let i = 0; i < close.length; i++) {
-        close[i].addEventListener('click', (e) => {
-            const row = close[i].parentNode.parentNode;
+  const deleteItem = document.querySelectorAll('.delete')
+    for (let i = 0; i < deleteItem.length; i++) {
+      deleteItem[i].addEventListener('click', (e) => {
+            const row = deleteItem[i].parentNode.parentNode;
             row.parentNode.removeChild(row);
             const indexToBeRemoved = productInCart.findIndex((product, index) => index === i);
             productInCart.splice(indexToBeRemoved, 1);
+            decrement()
         });
     }
 }
